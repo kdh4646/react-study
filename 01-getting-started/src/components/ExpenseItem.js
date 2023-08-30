847b04f6d736af92eea0aa8a13d624a688a71e1b
Derek Kweon <kdh4646@hanmail.net>
2023-08-30 16:02:24 -0700

    2023-08-30 variable injection through {}

--- 01-getting-started/src/components/ExpenseItem.js
+++ 01-getting-started/src/components/ExpenseItem.js
@@ -4,14 +4,20 @@ import './ExpenseItem.css';
     A Component is React is just a JavaScript Function (returning JSX)
     
     Use "className" instead of "class"
+
+    "{ ... }" for injection 
 */
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);  //2021-03-28
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
