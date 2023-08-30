92f55da450dd004a0dec4f3ac9104c53251f51ea
Derek Kweon <kdh4646@hanmail.net>
2023-08-29 20:28:13 -0700

    2023-08-29 : basic information about React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+/*
+  JSX (JavaScript XML)
+  
+  Only works because there are transformation steps running behind scenes.
+  (npm start process transform JavaScript code to more browser friendly code)
+*/
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
     </div>
   );
 }
