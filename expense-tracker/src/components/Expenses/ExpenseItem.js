5c645e63ad4adda010e6a8397dcc6f1391c7f5b9
Derek Kweon <kdh4646@hanmail.net>
2023-08-30 22:41:36 -0700

    2023-08-30 click handler

--- expense-tracker/src/components/Expenses/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -11,14 +11,21 @@ import './ExpenseItem.css';
 
     One parameter "props" for receiving data, key and value pairs
 */
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  let title = props.title;
+  
+  const clickHandler = () => {
+    title = 'Updated!';
+  };
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
