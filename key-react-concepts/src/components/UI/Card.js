57751dd2ca584081a9641c8029100f5a31f6456c
Derek Kweon <kdh4646@hanmail.net>
2023-08-30 21:53:51 -0700

    2023-08-30 Answer: more reusability

--- key-react-concepts/src/components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import './Card.css';
-
-function Card(props) {
-    const classes = 'concept ' + props.className; 
-
-    return <li className={classes}>{props.children}</li>;
-}
-
-export default Card;
